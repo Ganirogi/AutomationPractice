ab758092b4dc1654ca9231dd517e73d8d5eb5f29
andnita <AndreeaElena.Nita@endava.com>
2021-07-22 16:33:33 +0300

    add update passwd test

--- src/main/java/AutomationPractice/MyPersonalInformation.java
+++ src/main/java/AutomationPractice/MyPersonalInformation.java
@@ -8,37 +8,74 @@ import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class MyPersonalInformation extends BasePage{
-    public MyPersonalInformation(WebDriver webDriver){
-        super(webDriver);
-        PageFactory.initElements(getWebDriver(),this);
-    }
-
+public class MyPersonalInformation extends BasePage {
     @FindBy(className = "account")
     private WebElement accountName;
-
     @FindBy(id = "email")
     private WebElement email;
-
     @FindBy(id = "firstname")
     private WebElement firstName;
+    @FindBy(id = "old_passwd")
+    private WebElement oldPasswdInput;
+    @FindBy(id = "passwd")
+    private WebElement passwdInput;
+    @FindBy(id = "confirmation")
+    private WebElement confirmationPasswdInput;
+    @FindBy(xpath = "//*[@id=\"center_column\"]/div/form/fieldset/div[11]/button")
+    private WebElement saveButton;
 
-    public String getEmailText(){
+    public MyPersonalInformation(WebDriver webDriver) {
+        super(webDriver);
+        PageFactory.initElements(getWebDriver(), this);
+    }
+
+    public String getEmailText() {
         return getEmail().getAttribute("value");
     }
 
-    public String getFirstNameText(){
+    public String getFirstNameText() {
         return getFirstName().getAttribute("value");
     }
 
-    public YourAccount returnToYourAccount(){
+    public YourAccount returnToYourAccount() {
         getAccountName().click();
-        new WebDriverWait(getWebDriver(),5).until(ExpectedConditions.visibilityOfElementLocated(By.className("icon-user")));
+        new WebDriverWait(getWebDriver(), 5).until(ExpectedConditions.visibilityOfElementLocated(By.className("icon-user")));
 
         return new YourAccount(getWebDriver());
     }
 
-    public WebElement getEmail(){ return email; }
-    public WebElement getFirstName(){ return firstName; }
-    public WebElement getAccountName(){ return accountName; }
+    public YourAccount updatePassword(String oldPasswd, String newPasswd, String confirmationPasswd) {
+        getOldPasswdInput().sendKeys(oldPasswd);
+        getPasswdInput().sendKeys(newPasswd);
+        getConfirmationPasswdInput().sendKeys(confirmationPasswd);
+        return new YourAccount(getWebDriver());
+    }
+
+    public WebElement getEmail() {
+        return email;
+    }
+
+    public WebElement getFirstName() {
+        return firstName;
+    }
+
+    public WebElement getAccountName() {
+        return accountName;
+    }
+
+    public WebElement getOldPasswdInput() {
+        return oldPasswdInput;
+    }
+
+    public WebElement getPasswdInput() {
+        return passwdInput;
+    }
+
+    public WebElement getConfirmationPasswdInput() {
+        return confirmationPasswdInput;
+    }
+
+    public WebElement getSaveButton() {
+        return saveButton;
+    }
 }
