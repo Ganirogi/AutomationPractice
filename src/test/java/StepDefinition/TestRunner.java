4cf18f29d9cb2defc0a1612e8c2a683ee10d2635
Dedi121 <dedi.alex.121@gmail.com>
2021-08-24 18:00:22 +0300

    add

--- src/test/java/StepDefinition/TestRunner.java
+++ src/test/java/StepDefinition/TestRunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "Feature"
-        ,glue={"stepDefinition"}
+        ,glue={"StepDefinition"}
 )
 
 public class TestRunner {
