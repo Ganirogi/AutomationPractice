dc7ab5b5b85ca023a5bcb8b3206ee522d3ac56c3
Dinu Giorgian Vasile <dinugv94@gmail.com>
2021-08-25 13:05:55 +0300

    logintest update

--- src/test/java/QuickViewTest.java
+++ src/test/java/QuickViewTest.java
@@ -5,12 +5,13 @@ import org.junit.Test;
 public class QuickViewTest extends BaseTest{
 
     @Test
-    public void quickView() throws InterruptedException {
+    public void logIn() throws InterruptedException {
         goToLandingPage();
+
         SignInPage signIn = new SignInPage(getWebDriver());
         CreateOrLogIn createOrLogIn = signIn.clickSignIn();
         createOrLogIn.logIn("dinugv94@gmail.com", "P4ssword!");
-        YourAccount yourAccount = createOrLogIn.clickSignin();
+        YourAccount yourAccount = createOrLogIn.signIn();
         yourAccount.hoverOver();
         CasualDresses casualDresses = yourAccount.clickCasualDresses();
         QuickView quickView = casualDresses.clickQuickView();
@@ -22,3 +23,4 @@ public class QuickViewTest extends BaseTest{
         Assert.assertEquals("Total price test", cart.getTotalPay(), cart.getTotalPrice().getText());
     }
 }
+
